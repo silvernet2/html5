1dabd70a04334ab398a82c9099f8c937f71b729f
nataliamgu <67884213+nataliamgu@users.noreply.github.com>
2020-12-26 23:58:56 -0300

    cambio color back
    se cambia color backgorund mediante funciones en js

---
+++ change back color/script.js
@@ -0,0 +1,9 @@
+function rColor(){
+    document.body.style.backgroundColor ="red";
+}
+function vColor(){
+    document.body.style.backgroundColor ="green";
+}
+function aColor(){
+    document.body.style.backgroundColor ="yellow";
+}
